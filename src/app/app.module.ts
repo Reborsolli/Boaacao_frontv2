dfe440c22010b84c5c45d9a4acecaa0d8c0aaa65
Reborsolli <renata.ferreirapn@gmail.com>
2021-10-11 15:25:31 -0300

    boa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { OrderModule } from 'ngx-order-pipe';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-
+import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { FormsModule } from '@angular/forms';
-
-import { EquipeComponent } from './equipe/equipe.component';
-
-
+import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { AlertasComponent } from './alertas/alertas.component';
 
 
 
@@ -24,24 +29,29 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     AppComponent,
     MenuComponent,
     RodapeComponent,
-
+    EntrarComponent,
     CadastrarComponent,
     InicioComponent,
-    EquipeComponent,
-
-
-    PostagemDeleteComponent
-
+    TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent,
+    UserEditComponent,
+    AlertasComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ModalModule.forRoot(),
+    OrderModule
   ],
   providers: [{
-    provide:LocationStrategy,
-    useClass: HashLocationStrategy
+    provide:LocationStrategy, 
+    useClass:HashLocationStrategy
   }],
   bootstrap: [AppComponent]
 })
